204b6cdde9c3d0566b3d9dc5493284148a98f78b
Alessio Lunghi <alessio@gamaroff.co.uk>
2013-05-10 17:30:59 +0100

    using ejs template

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -13,14 +12,18 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3001);
+
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -28,8 +31,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Home Page
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
